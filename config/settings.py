af46442724df8afb79eafd4e94dd411aaec62ff8
unknown <karimovf_22@icloud.com>
2025-04-02 19:19:40 +0500

    base.html add

--- config/settings.py
+++ config/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'configapp.User'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / 'config/static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
